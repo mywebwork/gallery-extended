20da83ea26206f48b0bac3621f5e7de4cb02e728
vidanov <vidanov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-25 10:30:28 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/gallery-extended/tags/1.1@428579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gallery.php
@@ -0,0 +1,140 @@
+<?php
+/*
+Plugin Name: Gallery Extended
+Plugin URI: http://vidanov.com/gallery-extended
+Description: Additional standard Wordpress gallery capabilities
+Version: 1.0
+Author: Vidanov
+Author URI: http://vidanov.com
+*/
+
+
+remove_shortcode('gallery');
+add_shortcode('gallery', 'av_gallery_shortcode');
+
+/**
+ * The Gallery shortcode.
+ *
+ * This implements the functionality of the Gallery Shortcode for displaying
+ * WordPress images on a post.
+ *
+ * @since 2.5.0
+ *
+ * @param array $attr Attributes attributed to the shortcode.
+ * @return string HTML content to display gallery.
+ */
+
+
+
+function av_gallery_styles()
+{
+   if (!is_admin())
+   wp_enqueue_style('gallery-extended', WP_PLUGIN_URL .'/gallery-extended/gallery.css');
+}
+add_action('init','av_gallery_styles');
+add_shortcode('gallery', 'av_gallery_shortcode');
+
+function av_gallery_shortcode($attr) {
+	global $post;
+
+
+
+		
+
+
+	static $instance = 0;
+	$instance++;
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'DESC',  // CHANGED - order DESC
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail'
+	), $attr));
+
+	$id = intval($id);
+	$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+
+	$selector = "gallery";
+
+	$output = apply_filters('gallery_style', "
+		<div id='$selector' class='gallery galleryid-{$id}'>");
+
+
+
+	$i = 0;
+
+// CHANGED  -- FUNCTIONALITY FOR $start and $end in GALLERY
+	$start=0;
+	$end=100000;
+	$counter = -1;
+			if ( isset( $attr['start'] ) ) 
+			{ $start=$attr['start'];}
+			if ( isset( $attr['end'] ) ) 
+			{ $end=$attr['end'];}
+						
+// CHANGED
+
+	foreach ( $attachments as $id => $attachment ) {
+		
+		$counter++; // CHANGED
+		if ($counter>=$start and $counter<=$end and $id!=$thumb_id ) { // CHANGED
+		
+							$link = true ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false); // CHANGED - link to file
+					
+							$output .= "<{$itemtag} class='gallery-item'>";
+							$output .= "
+								<{$icontag} class='gallery-icon'>
+									$link
+								</{$icontag}>";
+							if ( $captiontag && trim($attachment->post_excerpt) ) {
+								$output .= "
+									<{$captiontag} class='gallery-caption'>
+									" . wptexturize($attachment->post_excerpt) . "
+									</{$captiontag}>";
+							}
+							$output .= "</{$itemtag}>";
+							if ( $columns > 0 && ++$i % $columns == 0 )
+								$output .= '<br style="clear: both" />';
+								
+		} // CHANGED  -- FUNCTIONALITY FOR $start and $end in GALLERY
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+		</div>\n";
+
+	return $output;
+}
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,64 @@
+=== Gallery-Extended ===
+Contributors: Alexey Vidanov
+Tags: plugin, posts, gallery,admin,image 
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 1.1
+
+This plugin adds to [gallery] short code the start and end attributes to enable slice the post gallery and show different parts of them.
+
+== Description ==
+
+With this plugin it is possible to put the part of the gallery in the post using start and stop attributes. 
+
+Example of use:
+Turn on HTML, put the code like this
+[gallery link=file start=1 end=3]
+Some content
+[gallery link=file start=4 end=6]
+Some other content
+[gallery link=file start=7]
+
+It will show the first 3 images, then some content, then the images from 4 to 6, then some other content and then the rest of the gallery. It could be useful for large posts with many images inside.
+Better CSS (in external file) and the oldest images at the top by default. You can set now default link mode for images (standard wordpress or always link images in the gallery to file or alway link to the attachment page)
+
+
+= Features =
+* start and end attributes to show selected part of the gallery and put some content between
+* you can add more picture to illustrate the post and do not output all of them in the gallery with simple trick
+* CSS style in the external file for better HTML
+* rel attribute for fancy box included
+* you can set for the site default link mode for images (standard wordpress or always link images in the gallery to file or alway link to the attachment page)
+
+== Installation ==
+
+1. Upload `gallery-extended` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. You can adapt gallery CSS file gallery-extended/gallery.css for your purpose.
+
+== Frequently Asked Questions ==
+If you have any questions or comments, please drop me an email: http://vidanov.com/ contact
+
+== Screenshots ==
+
+1. Admin page
+2. Frontend
+3. Options page
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+= 1.01 =
+* Fixed Call to undefined function get_post_thumbnail_id()  error 
+= 1.1 =
+* Added options page 
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Initial release
+= 1.01 =
+* Fixed Call to undefined function get_post_thumbnail_id()  error 
+= 1.1 =
+* Added options page 
